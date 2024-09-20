c6500963ab040b190b979bbc6e5aeaab01e5317e
league <maddoxseul@sbsd.net>
2024-09-19 20:05:46 -0700

    Initial commit

---
+++ src/GamePanel.java
@@ -0,0 +1,43 @@
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+	
+	public GamePanel() {
+		System.out.println(Main.width + " " + Main.height);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+
+import javax.swing.JFrame;
+
+public class Main {
+	GamePanel gamePanel = new GamePanel();
+
+	static Dimension size = Toolkit.getDefaultToolkit().getScreenSize(); 
+
+// width will store the width of the screen 
+	public static final int width = (int)size.getWidth(); 
+
+// height will store the height of the screen 
+	public static final int height = (int)size.getHeight();
+	
+	
+	JFrame frame;// = new JFrame();
+
+	public Main() {
+		frame = new JFrame("League Invaders");
+		System.out.println(width + " " + height); 
+
+		// frame.setTitle("")
+
+	}
+
+	public void setup() {
+		frame.addKeyListener(gamePanel);
+		frame.setSize(width, height);
+		frame.add(gamePanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.setup();
+	}
+	
+
+}
